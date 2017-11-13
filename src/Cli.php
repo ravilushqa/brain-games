4d2fe004eeb91f9ea33ea7a282deb17b6fb557de
ravilushqa <Vfvfgfgf777>
2017-11-13 21:07:28 +0300

    make even game

--- src/Cli.php
+++ src/Cli.php
@@ -6,15 +6,76 @@ namespace BrainGames\Cli;
 use function \cli\line;
 use function \cli\prompt;
 
+const ANSWER_COUNT = 3;
+
 /**
  * Run game
  *
- * @return null
+ * @return void
  */
 function run()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    return null;
+}
+
+/**
+ * Introduction for user
+ *
+ * @param string $task Task for user
+ *
+ * @return void
+ */
+function intro(string $task)
+{
+    line('Welcome to the Brain Game!');
+    line($task);
+    line();
+}
+
+/**
+ * Run the game
+ *
+ * @param callable $question   question for user
+ * @param callable $trueAnswer true answer
+ * @param string   $task       Task for user
+ * 
+ * @return void
+ */
+function game(callable $question, callable $trueAnswer, string $task)
+{
+    intro($task);
+    $name = getName();
+
+    for ($i=0; $i < ANSWER_COUNT; $i++) {
+        $issue = $question();
+        line("Question: $issue");
+        $answer = prompt('Your answer');
+        if ($answer === $trueAnswer($issue)) {
+            line('Correct!');
+            continue;
+        } else {
+            // @codingStandardsIgnoreStart
+            line("'$answer' is wrong answer ;(. Correct answer was '{$trueAnswer($issue)}'.");
+            // @codingStandardsIgnoreEnd
+            line("Let's try again, $name");
+            return;
+        }
+    }
+    line("Congratulations, %s!", $name);
+    return;
+}
+
+/**
+ * Get user name
+ *
+ * @return string
+ */
+function getName()
+{
+    $name = ucfirst(prompt('May I have your name?'));
+    line("Hello, %s", $name);
+    line();
+    return $name;
 }
---
+++ src/Games/Even.php
@@ -0,0 +1,42 @@
+<?php
+// @codingStandardsIgnoreStart
+// @codingStandardsIgnoreEnd
+
+namespace BrainGames\Games\Even;
+use function \Braingames\Cli\game;
+
+const TASK = 'Answer "yes" if number even otherwise answer "no"';
+const MIN_VALUE = 1;
+const MAX_VALUE = 100;
+
+/**
+ * Runs game
+ *
+ * @return void;
+ */
+function run()
+{
+    $question = function () {
+        return rand(MIN_VALUE, MAX_VALUE);
+    };
+
+    $isCorrect = function ($num) {
+        return isEven($num);
+    };
+
+    game($question, $isCorrect, TASK);
+
+    return;
+}
+
+/**
+ * Checks for even number
+ *
+ * @param int $num The number to check
+ *
+ * @return string
+ */
+function isEven(int $num) : string
+{
+    return $num % 2 ? 'yes' : 'no';
+}
