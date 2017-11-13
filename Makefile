a0669752fa7b1b077d5e96f0f68da99ed744aa75
ravilushqa <Vfvfgfgf777>
2017-11-13 18:34:49 +0300

    added phpcs and fix for PSR

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 install:
 	composer install
+lint:
+	composer run-script phpcs src bin
+lint-fix:
+	composer run-script phpcbf
--- src/Cli.php
+++ src/Cli.php
@@ -5,8 +5,15 @@ namespace BrainGames\Cli;
 use function \cli\line;
 use function \cli\prompt;
 
-function run () {
+/**
+ * Run game
+ *
+ * @return null
+ */
+function run()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return null;
 }
