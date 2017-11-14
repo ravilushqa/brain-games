01fc9f9bef187f34d8b23229706616383ade5092
ravilushqa <Vfvfgfgf777>
2017-11-14 13:51:55 +0300

    make calc game

---
+++ src/Games/Calc.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace BrainGames\Games\Calc;
+use function \Braingames\Cli\game;
+
+const TASK = 'What is the result of the expression?';
+const MIN_VALUE = 1;
+const MAX_VALUE = 100;
+const SIGNS = ['+', '-', '*'];
+
+/**
+ * Runs game
+ *
+ * @return void;
+ */
+function run()
+{
+    $question = function () {
+        // @codingStandardsIgnoreStart
+        return rand(MIN_VALUE, MAX_VALUE) . ' ' . SIGNS[array_rand(SIGNS)] . ' ' . rand(MIN_VALUE, MAX_VALUE);
+        // @codingStandardsIgnoreEnd
+    };
+
+    $isCorrect = function (string $expression) {
+        return solveExpression($expression);
+    };
+
+    game($question, $isCorrect, TASK);
+
+    return;
+}
+
+/**
+ * Solve expression
+ *
+ * @param string $exp Solve expression
+ *
+ * @return mixed
+ */
+function solveExpression(string $exp) : string
+{
+    list($firstArgument, $sign, $secondArgument) = explode(" ", $exp);
+
+    switch ($sign)
+    {
+    case '+':
+        return $firstArgument + $secondArgument;
+            break;
+
+    case '-':
+        return $firstArgument - $secondArgument;
+            break;
+
+    case '*':
+        return $firstArgument * $secondArgument;
+            break;
+
+    case '/':
+        return $firstArgument / $secondArgument;
+            break;
+
+    default:
+        return "Sorry No command found";
+    }
+}
